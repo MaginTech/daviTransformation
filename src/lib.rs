4a8d9d8b191464efbdec8d0b3e09fd8ead53ed4e
Taiki-Ishigaki <ishigaki@ynl.t.u-tokyo.ac.jp>
2023-04-29 12:57:50 +0000

    [fix] indent

--- src/lib.rs
+++ src/lib.rs
@@ -38,41 +38,41 @@ pub fn read_json_file(path: &str) -> Result<DaviTreeNode, serde_json::Error> {
 
 #[cfg(test)]
 mod tests {
-    use super::*;
-    use serde_json::json;
+  use super::*;
+  use serde_json::json;
 
-    #[test]
-    fn test_davi_tree_node_default_values() {
-        let sample_json = json!({
-            "name": "node",
-            "id": 1,
-            "children": []
-        });
+  #[test]
+  fn test_davi_tree_node_default_values() {
+    let sample_json = json!({
+        "name": "node",
+        "id": 1,
+        "children": []
+    });
 
-        let parsed_node: DaviTreeNode = serde_json::from_value(sample_json).unwrap();
+    let parsed_node: DaviTreeNode = serde_json::from_value(sample_json).unwrap();
 
-        assert_eq!(parsed_node.name, "node");
-        assert_eq!(parsed_node.id, 1);
-        assert_eq!(parsed_node.position, default_translation3());
-        assert_eq!(parsed_node.rotation, default_rotation3());
-        assert!(parsed_node.quaternion.is_none());
-        assert!(parsed_node.children.unwrap().is_empty());
+    assert_eq!(parsed_node.name, "node");
+    assert_eq!(parsed_node.id, 1);
+    assert_eq!(parsed_node.position, default_translation3());
+    assert_eq!(parsed_node.rotation, default_rotation3());
+    assert!(parsed_node.quaternion.is_none());
+    assert!(parsed_node.children.unwrap().is_empty());
+  }
+
+  #[test]
+  fn test_read_json_file() {
+    let test_file_path = "./test_data/sample.json";
+
+    match read_json_file(test_file_path) {
+      Ok(tree) => {
+        assert_eq!(tree.name, "node");
+        assert_eq!(tree.id, 1);
+        assert_eq!(tree.position, default_translation3());
+        assert_eq!(tree.rotation, default_rotation3());
+        assert!(tree.quaternion.is_none());
+        assert!(tree.children.unwrap().is_empty());
+      }
+      Err(e) => panic!("Failed to parse JSON: {}", e),
     }
-
-    #[test]
-    fn test_read_json_file() {
-        let test_file_path = "./test_data/sample.json";
-
-        match read_json_file(test_file_path) {
-            Ok(tree) => {
-                assert_eq!(tree.name, "node");
-                assert_eq!(tree.id, 1);
-                assert_eq!(tree.position, default_translation3());
-                assert_eq!(tree.rotation, default_rotation3());
-                assert!(tree.quaternion.is_none());
-                assert!(tree.children.unwrap().is_empty());
-            }
-            Err(e) => panic!("Failed to parse JSON: {}", e),
-        }
-    }
+  }
 }
