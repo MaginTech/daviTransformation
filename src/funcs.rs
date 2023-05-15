7d6fe2fc0f27ffa984654c455d176aa04878d1ec
Taiki-Ishigaki <ishigaki@ynl.t.u-tokyo.ac.jp>
2023-05-15 15:42:11 +0000

    [add]

---
+++ src/funcs.rs
@@ -0,0 +1,61 @@
+use crate::deserialize::*;
+
+fn regist_node() {
+
+}
+
+fn request_root_translation(id: i32) -> Translation3<f64>{
+
+}
+
+fn request_root_rotation(id: i32) -> Rotation3<f64>{
+
+}
+
+fn request_world_translation(id: i32) -> Translation3<f64>{
+
+}
+
+fn request_world_rotation(id: i32) -> Rotation3<f64>{
+
+}
+
+fn request_local_translation(id: i32, local_id: i32) -> Translation3<f64>{
+
+}
+
+fn request_local_rotation(id: i32, local_id: i32) -> Rotation3<f64>{
+
+}
+
+fn update_traslation(id: i32, pos: Translation3<f64>){
+
+}
+
+fn update_rotation(id: i32, rot: Rotation3<f64>){
+
+}
+
+#[cfg(test)]
+mod tests {
+
+  #[test]
+  fn test_regist_node() {
+  }
+
+  #[test]
+  fn request_translation() {
+  }
+
+  #[test]
+  fn request_rotation() {
+  }
+
+  #[test]
+  fn update_rotation() {
+  }
+
+  #[test]
+  fn update_rotation() {
+  }
+}
