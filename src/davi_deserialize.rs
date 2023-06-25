fa03fa7d9ee24204de430cd551dac836e85ced4c
Taiki-Ishigaki <ishigaki@ynl.t.u-tokyo.ac.jp>
2023-06-25 08:20:51 +0000

    rename deserialize

--- src/davi_deserialize.rs
+++ src/davi_deserialize.rs
@@ -4,14 +4,30 @@ use serde_json;
 
 use crate::davi_tree::*;
 
+#[derive(Debug, Deserialize)]
+#[allow(dead_code)]
+pub struct DaviDesNode {
+  name: String,
+  id: i32,
+  children: Option<Vec<DaviTreeNode>>,
+
+  #[serde(default)]
+  position: Point3<f64>,
+  #[serde(default)]
+  rotation: Rotation3<f64>,
+}
+
+
 #[allow(dead_code)]
 pub fn read_json_file(path: &str) -> Result<DaviTreeNode, serde_json::Error> {
   let mut file = File::open(path).expect("Unable to open file");
   let mut contents = String::new();
   file.read_to_string(&mut contents).expect("Unable to read the file");
 
-  let tree: DaviTreeNode = serde_json::from_str(&contents)?;
-  Ok(tree)
+  let des: DaviDesNode = serde_json::from_str(&contents)?;
+  
+
+  Ok(davi)
 }
 
 #[cfg(test)]
