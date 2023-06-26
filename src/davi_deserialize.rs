242f3349accddf98d548d0ccb5b9d864ad7d61aa
Taiki-Ishigaki <ishigaki@ynl.t.u-tokyo.ac.jp>
2023-06-26 14:00:32 +0000

    update deserialize

--- src/davi_deserialize.rs
+++ src/davi_deserialize.rs
@@ -1,33 +1,32 @@
 use std::fs::File;
 use std::io::Read;
-use serde_json;
 
-use crate::davi_tree::*;
+use nalgebra::{Point3, Rotation3};
+
+use serde_json;
+use serde::{Deserialize};
 
 #[derive(Debug, Deserialize)]
 #[allow(dead_code)]
 pub struct DaviDesNode {
   name: String,
   id: i32,
-  children: Option<Vec<DaviTreeNode>>,
+  children: Option<Vec<DaviDesNode>>,
 
   #[serde(default)]
-  position: Point3<f64>,
+  rel_pos: Point3<f64>,
   #[serde(default)]
-  rotation: Rotation3<f64>,
+  rel_rot: Rotation3<f64>,
 }
 
-
 #[allow(dead_code)]
-pub fn read_json_file(path: &str) -> Result<DaviTreeNode, serde_json::Error> {
+pub fn read_json_file(path: &str) -> Result<DaviDesNode, serde_json::Error> {
   let mut file = File::open(path).expect("Unable to open file");
   let mut contents = String::new();
   file.read_to_string(&mut contents).expect("Unable to read the file");
 
   let des: DaviDesNode = serde_json::from_str(&contents)?;
-  
-
-  Ok(davi)
+  Ok(des)
 }
 
 #[cfg(test)]
@@ -37,16 +36,28 @@ mod tests {
 
   #[test]
   fn test_read_json_file() {
-    let test_file_path = "./test_data/sample.json";
+    let test_file_path = "./ext/sample.json";
 
     match read_json_file(test_file_path) {
       Ok(tree) => {
-        assert_eq!(tree.name, "node");
-        assert_eq!(tree.id, 1);
-        assert_eq!(tree.position, Point3::<f64>::origin());
-        assert_eq!(tree.rotation, Rotation3::<f64>::identity());
-        assert!(tree.children.unwrap().is_empty());
-      }
+        assert_eq!(tree.name, "origin");
+        assert_eq!(tree.id, 0);
+        assert_eq!(tree.rel_pos, Point3::<f64>::origin());
+        assert_eq!(tree.rel_rot, Rotation3::<f64>::identity());
+        match tree.children {
+          Some(children) => {
+            if let Some(child) = children.get(0){
+              assert_eq!(child.name, "robot1");
+              assert!(child.children.is_none());
+            }
+            if let Some(child) = children.get(1){
+              assert_eq!(child.name, "robot2");
+              assert!(child.children.is_none());
+            }
+          },
+          None => {},
+        }
+      },
       Err(e) => panic!("Failed to parse JSON: {}", e),
     }
   }
