0e1fc2ce68c51c58dcda1750816be14f370d77a7
Taiki-Ishigaki <ishigaki@ynl.t.u-tokyo.ac.jp>
2023-07-01 06:25:27 +0000

    add convert_to_davitree

--- src/davi_deserialize.rs
+++ src/davi_deserialize.rs
@@ -1,12 +1,16 @@
 use std::fs::File;
 use std::io::Read;
+use std::cell::RefCell;
+use std::rc::Rc;
 
-use nalgebra::{Point3, Rotation3};
+use nalgebra::{Point3, Rotation3, Vector6};
 
 use serde_json;
 use serde::{Deserialize};
 
-#[derive(Debug, Deserialize)]
+use crate::davi_tree::*;
+
+#[derive(Debug, Clone, Deserialize)]
 #[allow(dead_code)]
 pub struct DaviDesNode {
   name: String,
@@ -20,8 +24,30 @@ pub struct DaviDesNode {
 }
 
 #[allow(dead_code)]
-pub fn read_json_file(path: &str) -> Result<DaviDesNode, serde_json::Error> {
-  let mut file = File::open(path).expect("Unable to open file");
+// convert_to_davitree converts a DaviDesNode to a DaviTreeNode
+pub fn convert_to_davitree(s: DaviDesNode) -> Option<DaviTreeNode> {
+  let children = match s.children {
+    Some(child_nodes) => {
+      let converted_children: Vec<Rc<RefCell<DaviTreeNode>>>
+        = child_nodes.into_iter()
+                      .filter_map(|child| convert_to_davitree(child))
+                      .map(|node| Rc::new(RefCell::new(node))) 
+                      .collect();
+      Some(converted_children)
+    },
+    None => None,
+  };
+  
+  Some(DaviTreeNode {
+    name: s.name,
+    id: s.id,
+    children: children,
+    rel_pos: s.rel_pos,
+    rel_rot: s.rel_rot,
+    vel_twist: Vector6::<f64>::zeros(),
+    acc_twist: Vector6::<f64>::zeros()
+  })
+}
   let mut contents = String::new();
   file.read_to_string(&mut contents).expect("Unable to read the file");
 
@@ -35,6 +61,30 @@ mod tests {
   use nalgebra::{Point3, Rotation3};
 
   #[test]
+  // test_convert_to_davitree tests the convert_to_davitree function
+  fn test_convert_to_davitree() {
+    let des = DaviDesNode{
+      name: "origin".to_string(),
+      id: 0,
+      children: None,
+      rel_pos: Point3::<f64>::origin(),
+      rel_rot: Rotation3::<f64>::identity()
+    };
+
+    match convert_to_davitree(des){
+      Some(tree) => {
+        assert_eq!(tree.name, "origin");
+        assert_eq!(tree.id, 0);
+        assert_eq!(tree.rel_pos, Point3::<f64>::origin());
+        assert_eq!(tree.rel_rot, Rotation3::<f64>::identity());
+        assert!(tree.children.is_none());
+      },
+      None => {
+        assert!(false);
+      },
+    }
+  }
+
   fn test_read_json_file() {
     let test_file_path = "./ext/sample.json";
 
